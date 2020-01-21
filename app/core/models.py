1347ade17968a47b10065ec6a8482bdba3bb5cb5
ravaan <arpit.agarwal181@gmail.com>
2020-01-21 17:20:11 +0530

    divide line to conform to 70 char limit

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager, \
+    AbstractBaseUser, PermissionsMixin
 from django.db import models
 
 
